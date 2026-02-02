9a6dca3983a6a33accf16f88ecc9c0bbb6855ebe
NadaMamdouh225 <nada20011002@gmail.com>
2026-02-02 23:25:57 +0200

    Copy and Rename

---
+++ Copy_and_rename.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copy a file from one directory to another.
+cp docs/file.txt  dir0/
+
+# Rename a file in the same directory.
+mv file.txt newfile.txt
+
+# Copy a directory
+cp -r dir0/ docs/
+
+# Copy a file and rename it during the process
+cp test.txt project/newtest.txt
+
+
